fad57ac07837078e63ac4fe3c0bbdcf0708ee897
Nath2006 <ananthapindika739@gmail.com>
2025-04-12 02:04:37 +0700

    Refactor code structure for improved readability and maintainability

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import MarqueeSection from "./_components/ui/marquee";
+
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
@@ -14,6 +15,8 @@ export default function HomePage() {
             width={1250}
             height={1000}
             className="object-contain" 
+            quality={100}
+            priority
           />
         </div>
 
@@ -29,9 +32,64 @@ export default function HomePage() {
           </div>
         </div>
       </div>
-
       <div className="runningText mt-[90px]">
-       <MarqueeSection />
+        <MarqueeSection />
+      </div>
+      <div className="ContentWrapper bg-white w-full  h-screen rounded-t-[70px] mt-[56px]">
+      <div className="WhyUs mx-12  flex flex-col md:flex-row pt-[125px] justify-center items-center gap-[48px]">
+        <div className="ImageWrapper md:w-1/2">
+          {/* <Image
+            src="/assets/why-us-section.png"
+            alt="why us"
+            width={622}
+            height={400}
+            style={{ objectFit: "cover", width: "100%", height: "105px" }}
+          /> */}
+          <img
+            src="/assets/why-us-section.png"
+            alt="why us"
+            className="object-cover w-full"
+          />
+        </div>
+        <div className="TextWrapper w-1/2 flex flex-col">
+            <h1 className="font-bold text-[32px] text-[var(--font-color)] font-main">{t("WhyChooseUs.Title")}<br/>Academy?</h1>
+            <div className="contentWrapper pt-4 ">
+              <div className="flex items-center gap-4 py-2">
+              <div className="bg-blue-500 p-2 rounded-full w-[50px] h-[50px] flex justify-center items-center">
+                <p className="text-white font-medium text-[20px] font-main">1</p>
+              </div>
+                <p className="text-[var(--font-color)] font-medium font-main text-xl">{t("WhyChooseUs.why-one")}</p>
+              </div>
+              <div className="flex items-center gap-4 py-2">
+              <div className="bg-blue-500 p-2 rounded-full w-[50px] h-[50px] flex justify-center items-center">
+                <p className="text-white font-medium text-[20px] font-main">2</p>
+              </div>
+                <p className="text-[var(--font-color)] font-medium font-main text-xl">{t("WhyChooseUs.why-two")}</p>
+              </div>
+              <div className="flex items-center gap-4 py-2">
+              <div className="bg-blue-500 p-2 rounded-full w-[50px] h-[50px] flex justify-center items-center">
+                <p className="text-white font-medium text-[20px] font-main">3</p>
+              </div>
+                <p className="text-[var(--font-color)] font-medium font-main text-xl">{t("WhyChooseUs.why-three")}</p>
+              </div>
+              <div className="flex items-center gap-4 py-2">
+              <div className="bg-blue-500 p-2 rounded-full w-[50px] h-[50px] flex justify-center items-center">
+                <p className="text-white font-medium text-[20px] font-main">4</p>
+              </div>
+                <p className="text-[var(--font-color)] font-medium font-main text-xl">{t("WhyChooseUs.why-four")}</p>
+              </div>
+              <div className="flex items-center gap-4 py-2">
+              <div className="bg-blue-500 p-2 rounded-full w-[50px] h-[50px] flex justify-center items-center">
+                <p className="text-white font-medium text-[20px] font-main">5</p>
+              </div>
+                <p className="text-[var(--font-color)] font-medium font-main text-xl">{t("WhyChooseUs.why-five")}</p>
+              </div>
+              <button className="bg-blue-500 rounded-full py-3 px-8 mt-4 hover:bg-blue-600 transition duration-300">
+                <p className="text-white font-medium text-xl font-main">{t("WhyChooseUs.CTA")}</p>
+              </button>
+            </div>
+        </div>
+      </div>
       </div>
     </div>
   );
