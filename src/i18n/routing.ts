bc2989ceade46380e5c213435beb1f1d26aae60c
Nath2006 <ananthapindika739@gmail.com>
2025-04-30 09:51:51 +0700

    Feat: Add selection styling for improved text visibility and update routing comments for clarity

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,8 @@
 import {defineRouting} from 'next-intl/routing';
 import { createNavigation } from "next-intl/navigation";
 export const routing = defineRouting({
-  // A list of all locales that are supported
+  
+  //Bahasa Indoenesia and English is the supported locales
   locales: ['id', 'en'],
  
   // Used when no locale matches
